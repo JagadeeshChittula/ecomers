34269e43d37227536199bd12c120947c07a1f748
JagadeeshChittula <chittulaj@gmail.com>
2026-05-23 14:35:24 +0530

    fix: correct CORS middleware configuration in api.js

--- api.js
+++ api.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
-
+const cors = require("cors")
 
 
 const connectDB = require('./config/DBConnection');
 const productRoutes = require('./routes/productRoutes');
+
+const allowedorigins = [
+   "https://frontend-shopcart.vercel.app/",
+   "localhost:5173",
+  
+]
+
+app.use(cors({origins: allowedorigins
+   
+}
+))
 
 app.use(express.json());
 
